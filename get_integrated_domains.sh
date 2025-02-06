ee4c9cb4bb09bb3c256dc9f2ef4bc2f7129f2c57
peritob <peritob@users.noreply.github.com>
2025-02-06 21:41:39 +1100

    Update get_integrated_domains.sh

--- get_integrated_domains.sh
+++ get_integrated_domains.sh
@@ -9,7 +9,7 @@ grep 'Pfam' ${prefix_name}_augustus_aa.fasta.tsv > ${prefix_name}_pfam.tsv
 
 #Then extract all the genes based on the lists from NLR_clasification. For example,
 
-grep -w -f ${prefix_name}_${NLR_type}.list ${prefix_name}_pfam.tsv > ${prefix_name}_${NLR_type}_Pfams.tsv
+grep -F -w -f ${prefix_name}_${NLR_type}.list ${prefix_name}_pfam.tsv > ${prefix_name}_${NLR_type}_Pfams.tsv
 
 #Then extract all domains that are not present in NLR classifications using the parameter -v with fgrep. Make a list of NLR Pfams and call the txt file "NLR_Pfams.list" (PF00931, PF18052, PF08263, PF12799, PF13306, PF13855, PF13516, PF00560, PF07725, PF05659, PF01582, PF17862) associated with NLR genes.
 

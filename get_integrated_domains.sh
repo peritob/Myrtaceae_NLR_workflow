62b4ad0d75f1a5d38995fbd1ab08d07b2c42d48a
peritob <peritob@users.noreply.github.com>
2025-02-06 21:19:16 +1100

    Update get_integrated_domains.sh
    edit fgrep to grep -F

--- get_integrated_domains.sh
+++ get_integrated_domains.sh
@@ -9,11 +9,11 @@ grep 'Pfam' ${prefix_name}_augustus_aa.fasta.tsv > ${prefix_name}_pfam.tsv
 
 #Then extract all the genes based on the lists from NLR_clasification. For example,
 
-fgrep -w -f ${prefix_name}_${NLR_type}.list ${prefix_name}_pfam.tsv > ${prefix_name}_${NLR_type}_Pfams.tsv
+grep -F -w -f ${prefix_name}_${NLR_type}.list ${prefix_name}_pfam.tsv > ${prefix_name}_${NLR_type}_Pfams.tsv
 
 #Then extract all domains that are not present in NLR classifications using the parameter -v with fgrep. Make a list of NLR Pfams and call the txt file "NLR_Pfams.list" (PF00931, PF18052, PF08263, PF12799, PF13306, PF13855, PF13516, PF00560, PF07725, PF05659, PF01582, PF17862) associated with NLR genes.
 
-fgrep -wv -f NLR_Pfams.list ${prefix_name}_${NLR_type}_Pfams.tsv > ${prefix_name}_${NLR_type}_ID_Pfams.tsv
+grep -F -wv -f NLR_Pfams.list ${prefix_name}_${NLR_type}_Pfams.tsv > ${prefix_name}_${NLR_type}_ID_Pfams.tsv
 
 #Obtain meaningful column data 
 
